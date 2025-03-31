6324ce0abcdb89108604dc22818619be0c35eb86
EricHuangNZ <42832347+EricHuangNZ@users.noreply.github.com>
2025-03-31 17:16:22 +1300

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://drhuang.top' },
-    { title: 'Welcome to my blog', url: 'https://drhuang.top/en' }
+    { title: '欢迎来到我的博客', url: 'https://www.drhuang.top/about' },
+    { title: 'Welcome to my blog', url: 'https://www.drhuang.top/about' }
   ],
 
   // 英雄区左右侧组件颠倒位置
